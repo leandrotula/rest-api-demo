2e435e47b09b77c6cd977cf33704b40462b79fbb
Leandro Tula <leandrotula@gmail.com>
2019-09-21 22:24:41 -0300

    Added local profile to test

---
+++ src/main/resources/data-local.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (userId, firstName, lastName, email, encryptedPassword, emailVerificationToken, emailVerificationStatus) VALUES
+  ('id1', 'Leandro', 'Tula', 'ltula2@email.com', 'abcdefff8ka', 'abcdefff8ka', false),
+  ('id2', 'Julian', 'Cerrano', 'j2@email.com', 'rewqefff8ka', 'gtrdefff833', false),
+  ('id3', 'Sofia', 'Claro', 'sclaro@email.com', '99iuycefff8ka', 'ppdefyr8ka', false)
